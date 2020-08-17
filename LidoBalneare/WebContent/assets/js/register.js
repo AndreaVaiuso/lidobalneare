07fe7c6aafe0d855d50a327c927b69bf69eaf35d
Carlo <carlogalatolo@gmail.com>
2020-08-17 18:26:53 +0200

    register.js

--- LidoBalneare/WebContent/assets/js/register.js
+++ LidoBalneare/WebContent/assets/js/register.js
@@ -1,3 +1,18 @@
 $("#registerbuttonreg").click(
-		alert("register")
+		function(){
+			var email = document.getElementById("emailinputreg").value;
+			var password = document.getElementById("passwordinputreg").value;
+			var birthdate = $("#birthdatereg").val();
+			
+			console.log(username + " " + password);
+			
+			var userdata = {
+					"type" : "register",
+					"email" : email,
+					"password" : password,
+					"birthdate" : birthdate
+			};
+			
+			$.post("RegisterServlet", userdata, "json");
+		}
 );
