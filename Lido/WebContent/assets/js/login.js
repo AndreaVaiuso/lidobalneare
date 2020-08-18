f5bf2283027ac896e68a61a846c586ca68bbee00
Carlo <carlogalatolo@gmail.com>
2020-08-18 12:56:54 +0200

    showAlert

--- Lido/WebContent/assets/js/login.js
+++ Lido/WebContent/assets/js/login.js
@@ -1,26 +1,30 @@
-$("#loginbutton").click(	// Login button click event handler.
-		function(){
-			var email = $("#emailinput").val(); 
-			var password = $("#passwordinput").val();
-			console.log(email + " " + password);	// DEBUG
-			var userdata = {
-					"type" : "login",
-					"email" : email,
-					"password" : password,
-			};
-			$.post("LoginServlet",userdata,function(data,status,xhr){
-				if(data.type == "loginError"){
-					showerror("Login error","Your credentials do not match");
-				}
-				else if(data.type == "success"){
-					
-				}
-			},"json")
-		}
-);
+$(document).ready(function(){
 
-$("#registerbutton").click(
-		function(){
-			location.href = "register.html";
-		}
-);
+	$("#loginbutton").click(	// Login button click event handler.
+			function(){
+				var email = $("#emailinput").val(); 
+				var password = $("#passwordinput").val();
+				console.log(email + " " + password);	// DEBUG
+				var userdata = {
+						"type" : "login",
+						"email" : email,
+						"password" : password,
+				};
+				$.post("LoginServlet",userdata,function(data,status,xhr){
+					if(data.type == "loginError"){
+						showerror("Login error","Your credentials do not match");
+					}
+					else if(data.type == "success"){
+						
+					}
+				},"json")
+			}
+	);
+	
+	$("#registerbutton").click(
+			function(){
+				location.href = "register.html";
+			}
+	);
+
+});
